4e50d5f68b915d828c8d4894856fd50bf875ba75
yeremy lopez <yerlopez123@gmail.com>
2025-01-26 22:02:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log("ejecutando el sistema")
 
+//Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+let i = 1;  
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+
